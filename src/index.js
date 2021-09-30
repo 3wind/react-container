962136016203d0d53f95cbc3873bebf379d839c8
wangxin <wangxin15@sfmail.sf-express.com>
2021-09-30 17:57:35 +0800

    feat: 初始化

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import _ from 'lodash';
-import './style.css';
-
+import _ from "lodash";
+import printMe from "./print.js";
 function component() {
-  var element = document.createElement('div');
+  var element = document.createElement("div");
+  var btn = document.createElement("button");
+  element.innerHTML = _.join(["Hello", "webp333ac234k111"], " ");
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  element.appendChild(btn);
 
   return element;
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept("./print.js", function () {
+    console.log("Accepting the updated printMe module!");
+    printMe();
+  });
+}
